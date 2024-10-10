8c30e96c07da030e44621b42d6d4dcf514f4084e
BrendahBanks <ampurirebrendah7@gmail.com>
2024-10-10 10:40:58 +0300

    class work

---
+++ question1.py
@@ -0,0 +1,11 @@
+# Question 1 (a)
+#Create a python program that calculates the area of a triangle.
+base=4
+height=6
+area=1/2*base*height
+print(f"The area of a triangle of base 4 and height 6 is {area}")
+
+base=float(input("Enter the base: "))
+height=float(input("Enter the height: "))
+area=1/2*base*height
+print(f"The rea of a triangle is {area}")
---
+++ question2.py
@@ -0,0 +1,6 @@
+#Using a function create a program that calculates the volume of a cylinder.
+import math
+r=float(input("Enter the value of radius:"))
+h=float(input("Enter the value of height:"))
+volume_of_a_cylinder=22/7*r**2*h
+print(f"The volume of a cylinder is {volume_of_a_cylinder}")
